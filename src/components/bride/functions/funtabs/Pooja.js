6f8d8e52305748fa8e9511b022a2dce6e113ced9
2amlover <www.insultu@gmail.com>
2023-10-02 21:00:57 +0530

    v1 pooja

--- src/components/bride/functions/funtabs/Pooja.js
+++ src/components/bride/functions/funtabs/Pooja.js
@@ -1,20 +1,23 @@
 import React from "react";
 import PoojaImage from "../../../../assets/pooja.png";
-import "./pooja.css"; // Import a CSS file for styling
 
 const Pooja = () => {
+  const imageSize = {
+    maxHeight: "190px", // Set the maximum height (adjust this value as needed)
+  };
+
   return (
-    <div className="pooja-container">
-      <img src={PoojaImage} alt="" className="pooja-image" />
-      <div className="card border-warning mb-2 mt-2 rounded-0 pooja-card">
-        <div className="card-body text-start p-2">
-          <h5 className="names">Pooja</h5>
-          <p className="card-text mb-1">October 15, 2023, Tuesday</p>
-          <p className="card-text mb-1">11:30 AM</p>
-          <p className="card-text mb-2">Venue: Groom's Residence</p>
-          <button className="button">View Map</button>
-        </div>
+    <div>
+      <img src={PoojaImage} alt="" style={imageSize} />
+      <div className="card border-warning mb-2 mt-2 rounded-0" style={{ maxWidth: '18rem', marginLeft: '17px' }}>
+      <div className="card-body text-start p-2" style={{marginLeft:'10px'}}>
+        <h5 className="names">Pooja</h5>
+        <p className="card-text mb-1">October 15, 2023,Tuesday</p>
+        <p className="card-text mb-1">11:30 AM</p>
+        <p className="card-text mb-2">Venue: Groom's Residence</p>
+        <button className="button">View Map</button>
       </div>
+    </div>
     </div>
   );
 };
